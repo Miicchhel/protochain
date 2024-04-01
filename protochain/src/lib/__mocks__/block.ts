25e951d33f9d0a5c3967dc53a9450fd92aced121
Michel Ferreira da Silva Melo <michel.ferreira.melo@gmail.com>
2024-04-01 11:33:48 -0300

    feat: criando mock do block.ts -> lição 03: aula 06

---
+++ protochain/src/lib/__mocks__/block.ts
@@ -0,0 +1,45 @@
+import Validation from "../validation";
+
+/**
+ * Mock Block class
+ */
+export default class Block {
+    index: number;
+    timestamp: number;
+    previousHash: string;
+    data: string;
+    hash: string;
+
+    /**
+     * Mock Block constructor
+     * @param block the Mock block data
+     */
+    constructor(block?: Block) {
+            this.index = block?.index || 0;
+            this.timestamp = block?.timestamp || Date.now();
+            this.previousHash = block?.previousHash || "";
+            this.data = block?.data || "";
+            this.hash = block?.hash || this.getHash();
+    }
+
+    /**
+     * 
+     * @returns the Mock hash of the block
+     */
+    getHash(): string {
+        return this.hash || 'mockBlockHash'
+    }
+
+    /**
+    * Check if the Mock block is valid.
+    *
+    * @returns If the Mock block is valid, false otherwise
+    */
+    isValid(previousHash: string, previousIndex: number): Validation {
+
+        if(this.previousHash !== previousHash || previousIndex < 0 || this.index < 0) {
+            return new Validation(false, "The block index is invalid");
+        }
+        return new Validation();
+    }
+}

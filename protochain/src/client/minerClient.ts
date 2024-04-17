2edc3e45ac578d46854eda89de49e9d4348ed579
Michel Ferreira da Silva Melo <michel.ferreira.melo@gmail.com>
2024-04-17 18:02:23 -0300

    feat: criando a 1º versão do "Protominer", minerador da Protochain

---
+++ protochain/src/client/minerClient.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import Block from "../lib/block";
+import BlockInfo from "../lib/blockInfo";
+
+const BLOCKCHAIN_SERVER = "http://localhost:3000";
+const minerWallet = {
+    privateKey: "123456",
+    publicKey: "Michel_Melo_miner"
+};
+let totalMined = 0;
+
+console.log("Now we'll mine a new block!");
+async function mine() {
+    console.log("Fetching information for the next block...");
+    
+    const { data } = await axios.get(`${BLOCKCHAIN_SERVER}/blocks/next`);
+    
+    const blockinfo = data as BlockInfo;
+    
+    const newBlock = Block.fromBlockInfo(blockinfo);
+    
+    // TODO: adicionar tx de recompensa
+
+    console.log("Starting mining block #" + blockinfo.index);
+
+    newBlock.mine(blockinfo.difficulty, minerWallet.publicKey);
+    
+    console.log("block #" + blockinfo.index + " was mined! Sending it to the blockchain...");
+
+    try {
+        await axios.post(`${BLOCKCHAIN_SERVER}/blocks`, newBlock);
+        console.log('Block sent and accepted!');
+        totalMined++;
+        console.log("Total blocks mined: " + totalMined + "\n");
+    } catch (error: any) {
+        console.log(error.response ? error.response.data : error.message);
+    }
+    
+    setTimeout(() => {
+        mine();
+    }, 1000);
+}
+
+mine();

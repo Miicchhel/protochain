6eeb381fd0b097281260cac34b80a189e2e9e0d0
Michel Ferreira da Silva Melo <michel.ferreira.melo@gmail.com>
2024-04-20 14:43:36 -0300

    chore: ignorarando a cobertura para transactionType.ts

--- protochain/jest.config.ts
+++ protochain/jest.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "/src/lib/blockInfo.ts",
+    "/src/lib/transactionType.ts",
   ],
 
   // Indicates which provider should be used to instrument code for coverage
